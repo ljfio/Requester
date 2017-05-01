9048aad13275efabd97412edb0370620c5e4a41b
Luke Fisher <mail@lukefisher.me>
2017-05-01 15:40:38 +0100

    Add in some unit tests for the builder

--- library/src/androidTest/java/io/ljf/requester/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/io/ljf/requester/ExampleInstrumentedTest.java
@@ -4,8 +4,16 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.Semaphore;
 
 import static org.junit.Assert.*;
 
@@ -22,5 +30,46 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("io.ljf.requester", appContext.getPackageName());
+    }
+
+    @Test
+    public void builder_works() throws Exception {
+        final Semaphore mutex = new Semaphore(0);
+
+        Context context = InstrumentationRegistry.getTargetContext();
+
+        RequestBuilder<JsonObject, ?> builder = RequestBuilder.get(JsonObject.class);
+
+        builder.setUrl("http://echo.jsontest.com/key/value");
+
+        builder.setListener(new Response.Listener<JsonObject>() {
+                @Override
+                public void onResponse(JsonObject response) {
+                    JsonElement key = response.get("key");
+
+                    String value = key.getAsString();
+
+                    assertEquals("value", value);
+
+                    mutex.release();
+                }
+            }
+        );
+
+        builder.setErrorListener(new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                fail(error.getMessage());
+                mutex.release();
+            }
+        });
+
+        Request<JsonObject> request = builder.build();
+
+        RequestWorker worker = RequestWorker.getInstance(context);
+
+        worker.addToRequestQueue(request);
+
+        mutex.acquire();
     }
 }
